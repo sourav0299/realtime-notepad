bc60e592690fb3ae2a1955af62d6fe89561073ee
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2024-10-15 02:41:42 +0530

    Fix:

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <GitHubButton repoUrl="https://github.com/sourav0299/realtime-notepad" />
+      
       <Notepad />
     </div>
   );
