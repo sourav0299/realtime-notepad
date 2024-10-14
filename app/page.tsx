5c5d540f007e79b94b2df6b4ad8afe02af5a1af0
sourav0299 <105085605+sourav0299@users.noreply.github.com>
2024-10-15 02:31:37 +0530

    Feature: added ui components and add git button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 "use client"
 // app/page.tsx
 import Notepad from './components/Notepad';
+import GitHubButton from '../app/components/GitHubButton'
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
+      <GitHubButton repoUrl="https://github.com/yourusername/your-repo-name" />
       <Notepad />
     </div>
   );
